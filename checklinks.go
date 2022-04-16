84a53610cba24c60f87b39e07ed3ea144799d3df
Patrick Bucher <patrick.bucher@mailbox.org>
2022-04-16 13:53:05 +0200

    refactored code: moved logic from main program to module

--- checklinks.go
+++ checklinks.go
@@ -1,12 +1,17 @@
 package checklinks
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 
 	"golang.org/x/net/html"
 )
+
+var errNotCrawlable = errors.New("not crawlable")
 
 // FetchDocument gets the document indicated by the given url using the given
 // client, and returns its root (document) node. An error is returned if the
@@ -55,4 +60,152 @@ func QualifyInternalURL(page, link *url.URL) *url.URL {
 		// TODO: Query Parameters?
 	}
 	return qualifiedURL
+}
+
+// Link represents a link (URL) in the context of a web site (Site).
+type Link struct {
+	URL  *url.URL
+	Site *url.URL
+}
+
+// NewLink creates a Link from the given address. An error is returned, if the
+// address cannot be parsed.
+func NewLink(address string, site *url.URL) (*Link, error) {
+	u, err := url.Parse(address)
+	if err != nil {
+		return nil, err
+	}
+	return &Link{URL: u, Site: site}, nil
+}
+
+// IsInternal returns true if the link's URL points to the same domain as its
+// site, and false otherwise.
+func (l *Link) IsInternal() bool {
+	return l.URL.Hostname() == l.Site.Hostname() || l.URL.Hostname() == ""
+}
+
+// IsCrawlable returns true if the URL of the link has http(s) as the protocol,
+// or no protocol at all (which indicates an internal link), and false
+// otherwise.
+func (l *Link) IsCrawlable() bool {
+	return l.URL.Scheme == "https" || l.URL.Scheme == "http" || l.URL.Scheme == ""
+}
+
+// Result describes the result of processing a Link.
+type Result struct {
+	Err  error
+	Link *Link
+}
+
+// String returns a string prefixed with FAIL in case of an error, and prefixed
+// with OK if no error is present. The URL and error (if any) is contained in
+// the string.
+func (c Result) String() string {
+	if c.Err != nil {
+		return fmt.Sprintf(`FAIL "%s": %v`, c.Link.URL.String(), c.Err)
+	} else {
+		return fmt.Sprintf(`OK "%s"`, c.Link.URL.String())
+	}
+}
+
+// Sink represents a writable channel to which empty structs are written.
+type Sink chan<- struct{}
+
+// CrawlPage crawls the given site's URL and reports successfully checked
+// links, ignored links, and failed links (according to the flags ok, ignore,
+// fail, respectively). The given timeout is used to limit the waiting time of
+// the http client for a request.
+func CrawlPage(site *url.URL, timeout int, ok, ignore, fail bool) {
+	var wg sync.WaitGroup
+	links := make(chan *Link)
+	results := make(chan *Result)
+	done := make(chan struct{})
+
+	client := &http.Client{
+		Timeout: time.Duration(timeout) * time.Second,
+	}
+
+	go func() {
+		visited := make(map[string]struct{})
+		for {
+			select {
+			case l := <-links:
+				u := l.URL.String()
+				visited[u] = struct{}{}
+				if l.IsInternal() {
+					l.URL = QualifyInternalURL(site, l.URL)
+					wg.Add(1)
+					go ProcessNode(client, l, links, results, done)
+				} else {
+					wg.Add(1)
+					go ProcessLeaf(client, l, results, done)
+				}
+			case result := <-results:
+				if result.Err != nil {
+					if errors.Is(result.Err, errNotCrawlable) {
+						if ignore {
+							fmt.Println(result)
+						}
+					} else if fail {
+						fmt.Println(result)
+					}
+				}
+				if result.Err == nil && ok {
+					fmt.Println(result)
+				}
+			case <-done:
+				wg.Done()
+			}
+		}
+	}()
+
+	links <- &Link{site, site}
+	wg.Wait()
+}
+
+// ProcessNode uses the given http.Client to fetch the given site, and reports
+// the extracted links on the page (indicated by <a href="...">). Links
+// unsuitable for further crawling and malformed links are reported. A message
+// is sent to the given done channel when the node has been processed.
+func ProcessNode(c *http.Client, site *Link, links chan<- *Link, results chan<- *Result, done Sink) {
+	u := site.URL.String()
+	doc, err := FetchDocument(u, c)
+	if err != nil {
+		results <- &Result{Err: err, Link: site}
+		done <- struct{}{}
+		return
+	}
+	hrefs := ExtractTagAttribute(doc, "a", "href")
+	for _, href := range hrefs {
+		link, err := NewLink(href, site.Site)
+		if err != nil {
+			results <- &Result{Err: err, Link: site}
+			continue
+		}
+		if !link.IsCrawlable() {
+			results <- &Result{Err: errNotCrawlable, Link: site}
+			continue
+		}
+		links <- link
+	}
+	results <- &Result{Err: nil, Link: site}
+	done <- struct{}{}
+}
+
+// ProcessLeaf uses the given http.Client to fetch the given link using a HEAD
+// request, and reports the result of that request. A message is sent to the
+// given done channel when the node has been processed.
+func ProcessLeaf(c *http.Client, link *Link, results chan<- *Result, done chan<- struct{}) {
+	u := link.URL.String()
+	response, err := c.Head(u)
+	if err != nil {
+		results <- &Result{fmt.Errorf("HEAD %v %s", err, u), link}
+	} else if response.StatusCode != http.StatusOK {
+		statusCode := response.StatusCode
+		statusText := http.StatusText(statusCode)
+		results <- &Result{fmt.Errorf("HEAD %d %s %s", statusCode, statusText, u), link}
+	} else {
+		results <- &Result{nil, link}
+	}
+	done <- struct{}{}
 }
--- cmd/checklinks.go
+++ cmd/checklinks.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"net/http"
 	"net/url"
 	"os"
 	"strings"
-	"sync"
-	"time"
 
 	"github.com/patrickbucher/checklinks"
 )
@@ -19,8 +15,6 @@ var (
 	reportSucceeded = flag.Bool("success", false, "report succeeded links (OK)")
 	reportIgnored   = flag.Bool("ignored", false, "report ignored links (e.g. mailto:...)")
 	reportFailed    = flag.Bool("failed", true, "report failed links (e.g. 404)")
-
-	errNotCrawlable = errors.New("not crawlable")
 )
 
 func main() {
@@ -39,127 +33,5 @@ func main() {
 		fmt.Fprintf(os.Stderr, "parse %s as URL: %v", pageAddr, err)
 		os.Exit(1)
 	}
-	crawlPage(pageURL)
-}
-
-type Link struct {
-	URL  *url.URL
-	Site *url.URL
-}
-
-func NewLink(address string, site *url.URL) (*Link, error) {
-	u, err := url.Parse(address)
-	if err != nil {
-		return nil, err
-	}
-	return &Link{URL: u, Site: site}, nil
-}
-
-func (l *Link) IsInternal() bool {
-	return l.URL.Hostname() == l.Site.Hostname() || l.URL.Hostname() == ""
-}
-
-func (l *Link) IsCrawlable() bool {
-	return l.URL.Scheme == "https" || l.URL.Scheme == "http" || l.URL.Scheme == ""
-}
-
-type Result struct {
-	Err  error
-	Link *Link
-}
-
-func (c Result) String() string {
-	if c.Err != nil {
-		return fmt.Sprintf(`FAIL "%s": %v`, c.Link.URL.String(), c.Err)
-	} else {
-		return fmt.Sprintf(`OK "%s"`, c.Link.URL.String())
-	}
-}
-
-type Sink chan<- struct{}
-
-func crawlPage(site *url.URL) {
-	var wg sync.WaitGroup
-	links := make(chan *Link)
-	results := make(chan *Result)
-	done := make(chan struct{})
-
-	client := &http.Client{Timeout: time.Duration(*timeout) * time.Second}
-
-	go func() {
-		visited := make(map[string]struct{})
-		for {
-			select {
-			case l := <-links:
-				u := l.URL.String()
-				visited[u] = struct{}{}
-				if l.IsInternal() {
-					l.URL = checklinks.QualifyInternalURL(site, l.URL)
-					wg.Add(1)
-					go extractLinks(client, l, links, results, done)
-				} else {
-					wg.Add(1)
-					go checkHead(client, l, results, done)
-				}
-			case result := <-results:
-				if result.Err != nil {
-					if errors.Is(result.Err, errNotCrawlable) {
-						if *reportIgnored {
-							fmt.Println(result)
-						}
-					} else if *reportFailed {
-						fmt.Println(result)
-					}
-				}
-				if result.Err == nil && *reportSucceeded {
-					fmt.Println(result)
-				}
-			case <-done:
-				wg.Done()
-			}
-		}
-	}()
-
-	links <- &Link{site, site}
-	wg.Wait()
-}
-
-func extractLinks(c *http.Client, site *Link, links chan<- *Link, results chan<- *Result, done Sink) {
-	u := site.URL.String()
-	doc, err := checklinks.FetchDocument(u, c)
-	if err != nil {
-		results <- &Result{Err: err, Link: site}
-		done <- struct{}{}
-		return
-	}
-	hrefs := checklinks.ExtractTagAttribute(doc, "a", "href")
-	for _, href := range hrefs {
-		link, err := NewLink(href, site.Site)
-		if err != nil {
-			results <- &Result{Err: err, Link: site}
-			continue
-		}
-		if !link.IsCrawlable() {
-			results <- &Result{Err: errNotCrawlable, Link: site}
-			continue
-		}
-		links <- link
-	}
-	results <- &Result{Err: nil, Link: site}
-	done <- struct{}{}
-}
-
-func checkHead(c *http.Client, link *Link, results chan<- *Result, done chan<- struct{}) {
-	u := link.URL.String()
-	response, err := c.Head(u)
-	if err != nil {
-		results <- &Result{fmt.Errorf("HEAD %v %s", err, u), link}
-	} else if response.StatusCode != http.StatusOK {
-		statusCode := response.StatusCode
-		statusText := http.StatusText(statusCode)
-		results <- &Result{fmt.Errorf("HEAD %d %s %s", statusCode, statusText, u), link}
-	} else {
-		results <- &Result{nil, link}
-	}
-	done <- struct{}{}
+	checklinks.CrawlPage(pageURL, *timeout, *reportSucceeded, *reportIgnored, *reportFailed)
 }
